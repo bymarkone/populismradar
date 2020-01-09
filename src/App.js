78ed054bff9ee5b3e726e66eb3cdb3da277bbbc2
Marcos Silveira <bymarkone@gmail.com>
2020-01-09 15:06:37 +0100

    Initial commit for populismalert

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ResponsiveGeoMap } from '@nivo/geo'
+import Countries from './world_countries'
 
 function App() {
+  console.log(Countries.features)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveGeoMap
+          features={ Countries.features }
+          margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          projectionScale={181}
+          projectionTranslation={[ 0.4, 0.5 ]}
+          projectionRotation={[ 70, 360, 0 ]}
+          fillColor="#eeeeee"
+          borderWidth={1}
+          borderColor="#333333"
+          enableGraticule={true}
+          graticuleLineColor="#666666"
+      />
     </div>
   );
 }
